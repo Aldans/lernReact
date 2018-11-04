38bf48384f308e30c705c8d0c85a1d3f823a9bc0
All Don <seopakc.05@gmail.com>
2018-11-05 00:16:13 +0200

    state-part-3-1-использование состояния в виде хранилища

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import './App.css';
 import Car from './Components/Car/Car';
 
 class App extends Component {
+  state = {
+    cars: [
+      {name: "Mazda", year: 2016},
+      {name: "Ford", year: 2014},
+      {name: "Tesla", year: 2018},
+      {name: "Ferrari", year: 2010},
+    ],
+    pageTitle: "Cards Car"
+  }
+
+  render() { 
+
+   const car = this.state.cars
   
-  render() {
-    return (
-      <div className="App">
-        <Car name={'Mazda'} year={2018} colorz={'Red'} />
-        <Car name={'Ford'} year={2013} colorz={'Blue'} />
-        <Car name={'Tesla'} year={2017} colorz={'White'} />
-        <Car name={'Ferrari'} year={2015} colorz={'Red'} />
-      </div>
-    );
+   return (
+    <div className="App">
+     <h3>{this.state.pageTitle}</h3>
+     <Car name={ car[0].name} year={ car[0].year} colorz={'Red'} />
+     <Car name={ car[1].name} year={ car[1].year} colorz={'Blue'} />
+     <Car name={ car[2].name} year={ car[2].year} colorz={'White'} />
+     <Car name={ car[3].name} year={ car[3].year} colorz={'Green'} />
+    </div>
+   );
   }
 }
 export default App;
