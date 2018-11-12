09f4a1b05837e5faf4408c713c530785520d3b1c
All Don <seopakc.05@gmail.com>
2018-11-13 00:05:29 +0200

    динамическое добавление данных из состояния part 3-6

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+// Динамическое использование данных part 3-6
+// https://185.48.4.131/remote.php/webdav/NAS/Tor/%5Bwebformyself%5D-%D0%91%D0%B8%D0%B1%D0%BB%D0%B8%D0%BE%D1%82%D0%B5%D0%BA%D0%B0-ReactJS/videokurs/part3/6.mp4
+
 import React, { Component } from 'react';
 import './App.sass';
 import Car from './Components/Car/Car';
@@ -13,9 +16,9 @@ class App extends Component {
     pageTitle: "Cards Car"
   }
 
-  // функция меняющая тайтл
+  // функция меняющая тайтл параметр принимает (car.name)
   changeTopTitleHandler = (newTitle) => {
-    this.setState({
+    this.setState({ //Меняет текущий state
       pageTitle: newTitle
     })
   }
@@ -29,8 +32,6 @@ class App extends Component {
 
   render() { 
 
-   const car = this.state.cars
-  
    return (
 
     <div className="App">
@@ -40,30 +41,26 @@ class App extends Component {
 
       <div className="wrap-btn">
 
-        <input type="text" onChange={this.hengledInp}/>
+        <input type="text" onChange={ this.hengledInp }/>
 
-        <button className="butTitle" onClick={this.changeTopTitleHandler.bind(this, ' changed ' )}>Change title</button>
+        <button className="butTitle" onClick={this.changeTopTitleHandler.bind(this, ' Changed! ' )}>Change title</button>
         {/* 'changed' это передаваемый параметр -> 'props' */}
       </div>
 
       <div className="wrap-carList">
         <div className="carList">
-         <Car name={car[0].name} 
-              year={car[0].year} 
-              colorz={'Red'} 
-              onChanegTit={ this.changeTopTitleHandler.bind(this, car[0].name) } />
-         <Car name={car[1].name} 
-              year={car[1].year} 
-              colorz={'Blue'} 
-              onChanegTit={ this.changeTopTitleHandler.bind(this, car[1].name) } />
-         <Car name={car[2].name} 
-              year={car[2].year} 
-              colorz={'White'} 
-              onChanegTit={ this.changeTopTitleHandler.bind(this, car[2].name) } />
-         <Car name={car[3].name} 
-              year={car[3].year} 
-              colorz={'Green'} 
-              onChanegTit={ this.changeTopTitleHandler.bind(this, car[3].name) } />
+          {/* динамический вывод из state обькта(списка авто) */}
+          {this.state.cars.map((car, index) => {
+            return (
+              <Car
+                key={ index } // Уникальный индекс для рендеренга нужного елемнта
+                name={car.name}
+                year={car.year}
+                onChanegTit={ () => this.changeTopTitleHandler(car.name) } //Функция колбек
+              />
+            )
+          }) }    
+
         </div>
       </div>
 
